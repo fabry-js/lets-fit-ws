caea47d3820dcc48d3329aeb5448ba2071480198
Fabrizio Piperno <vivaldifabrizio10@gmail.com>
2021-03-16 12:44:16 +0100

    chore:eslint warns

--- restaurant-dashboard/src/providers/AuthProvider.tsx
+++ restaurant-dashboard/src/providers/AuthProvider.tsx
@@ -1,6 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { SpecialLoginResolveData, _auth } from '../utils/firebase';
 import { getCurrentUserInfos } from "../store/slices/userSlice";
 
 interface AuthProviderProps {}
@@ -15,7 +14,7 @@ const AuthProvider: React.FC<AuthProviderProps> = ({children}) =>{
       setActualUser(user)
     :
       setActualUser(null);
-  });
+  }, [user]);
 
   return (
     <UserContext.Provider value={{ actualUser }}>
