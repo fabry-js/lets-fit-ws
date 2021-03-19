ac7b06eb9e7f92449a00d8aca23993f0efd54281
Fabrizio Piperno <vivaldifabrizio10@gmail.com>
2021-03-19 19:13:22 +0100

    added error handling

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const stripe = require('stripe')('sk_test_51IJJPcIzkBXCvUW1usTx6HMp1Rh2bjoWM1TOyoA05BlP9qySrnUrQ8wjq1CCIEIJie6XjLvEJsYxZEbxP5Uo7GmD00EojT4de0')
-const cors = require('cors');
+const express = require("express");
+const stripe = require("stripe")(
+  "sk_test_51IJJPcIzkBXCvUW1usTx6HMp1Rh2bjoWM1TOyoA05BlP9qySrnUrQ8wjq1CCIEIJie6XjLvEJsYxZEbxP5Uo7GmD00EojT4de0"
+);
+const cors = require("cors");
 
 const app = express();
 
@@ -13,25 +15,31 @@ const calculateOrderAmount = (items) => {
   });
 
   return accumTotal;
-}
+};
 
 app.get("/", async (_req, res) => {
   res.send({
-    message: "Salve!"
-  })
-})
+    message: "Salve!",
+  });
+});
 
 app.post("/create-payment-intent", async (req, res) => {
-  const { ingredients } = req.body;
-  console.log(ingredients);
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(ingredients),
-    currency: "eur"
-  });
+  if (req.body.ingredients) {
+    const { ingredients } = req.body;
+    console.log(ingredients);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(ingredients),
+      currency: "eur",
+    });
 
-  res.send({
-    clientSecret: paymentIntent.clientSecret
-  })
+    res.send({
+      clientSecret: paymentIntent.clientSecret,
+    });
+  } else {
+    res.send({
+      message: "no ingredients",
+    });
+  }
 });
 
 app.listen(process.env.PORT || 5000, () => console.log("pronto."));
