3c951a9d358adc9696aa60ea255e4ee2c693c4d5
Fabrizio Piperno <vivaldifabrizio10@gmail.com>
2021-03-20 08:25:52 +0100

    emergency fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(express.static());
+app.use(express.static("."));
 app.use(express.json());
 
 const calculateOrderAmount = (items) => {
