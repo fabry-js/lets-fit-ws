a6ded05d73a9cef3af672d05d8067f290b85ab36
Fabrizio Piperno <vivaldifabrizio10@gmail.com>
2021-04-19 15:57:00 +0200

    [!fix]: CORS

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const stripe = new Stripe(
 
 const app = express();
 
-app.use(cors({ origin: "https://www.letsfitja.com" }));
+app.use(cors({ origin: "*" }));
 app.use(express.static("."));
 app.use(express.json());
 
