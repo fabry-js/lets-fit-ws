fd4650bad6771dd53e07db9a7f390bd10a591666
Fabrizio Piperno <vivaldifabrizio10@gmail.com>
2021-04-22 12:39:54 +0200

    feat: call to serever

--- src/pages/blog/newsletter/NewsletterSubscription.tsx
+++ src/pages/blog/newsletter/NewsletterSubscription.tsx
@@ -3,19 +3,45 @@ import { Button } from "@chakra-ui/button";
 import { Input } from "@chakra-ui/input";
 import { Box, Text } from "@chakra-ui/layout";
 import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useToast } from "@chakra-ui/toast";
 
 interface NewsletterSubscriptionProps {}
 
 const NewsletterSubscription: React.FC<NewsletterSubscriptionProps> = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data: any) => {
-    console.log(data);
+  const toast = useToast();
+  const onSubmit = (data: {
+    email: string,
+    userName: string
+  }) => {
+    const { email, userName } = data;
+    axios.post("http://localhost:5000/ns-sub", {
+      email,
+      userName
+    }).then((res) => {
+      if (res.data.message === "successful") {
+        toast({
+          title: "Email Iscritta con successo!",
+          isClosable: true,
+          duration: 2000,
+          status: "success"
+        })
+      } else if (res.data.error === "user-already-subscribed") {
+        toast({
+          title: "Email già iscritta nel nostro sistema!",
+          isClosable: true,
+          duration: 2000,
+          status: "error"
+        })
+      }
+    })
   };
   return (
     <Box p="7%">
       <Text>
         Heyla, 👋! Se sei interessat* alla nostra newsletter settimanale,
-        inserisci qui la tua email!
+        inserisci qui la tua email e il tuo nome!
       </Text>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div
@@ -24,7 +50,10 @@ const NewsletterSubscription: React.FC<NewsletterSubscriptionProps> = () => {
             placeItems: "center",
           }}
         >
-          <Input name="email" ref={register} placeholder="E-Mail" required />
+          <Input name
+          ="userName" ref={register} placeholder="Nome" required />
+          <Input name
+          ="email" ref={register} placeholder="E-Mail" required />
           <Button mt="2%" type="submit" value="submit">
             Iscrivimi!
           </Button>
