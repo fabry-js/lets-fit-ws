54f673f67462e048101bf1c52b2c755ba618fc4e
Fabrizio Piperno <vivaldifabrizio10@gmail.com>
2021-04-25 17:31:13 +0200

    feat: better mobile responsiveness on fasi

--- src/pages/shop-components/main-section/routers/FasiTabs.tsx
+++ src/pages/shop-components/main-section/routers/FasiTabs.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { SimpleGrid, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import {
+  SimpleGrid,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
 import FaseCarboidrati from "../fasi/FaseCarboidrati";
 import FaseGrassi from "../fasi/FaseGrassi";
 import FaseProteine from "../fasi/FaseProteine";
@@ -9,18 +16,33 @@ import FaseBibite from "../fasi/FaseBibite";
 import { useMedia } from "use-media";
 
 const FasiTabs = () => {
-  const isWideScreen = useMedia({ minWidth: 1000 })
+  const isWideScreen = useMedia({ minWidth: 1000 });
   return (
-    <Tabs variant="soft-rounded" orientation={isWideScreen ? "vertical": "horizontal"} colorScheme="whatsapp">
+    <Tabs
+      variant="soft-rounded"
+      orientation={isWideScreen ? "vertical" : "horizontal"}
+      colorScheme="whatsapp"
+    >
       <TabList>
-      <SimpleGrid columns={[2, 4]} columnGap="2%">
-        <Tab>Carboidrati</Tab>
-        <Tab>Proteine</Tab>
-        <Tab>Grassi</Tab>
-        <Tab>Piatti Pronti</Tab>
-        <Tab>Contorni</Tab>
-        <Tab>Bibite</Tab>
-        </SimpleGrid>
+        {isWideScreen ? (
+          <>
+            <Tab>Carboidrati</Tab>
+            <Tab>Proteine</Tab>
+            <Tab>Grassi</Tab>
+            <Tab>Piatti Pronti</Tab>
+            <Tab>Contorni</Tab>
+            <Tab>Bibite</Tab>
+          </>
+        ) : (
+          <SimpleGrid columns={[2, 4]} columnGap="2%">
+            <Tab>Carboidrati</Tab>
+            <Tab>Proteine</Tab>
+            <Tab>Grassi</Tab>
+            <Tab>Piatti Pronti</Tab>
+            <Tab>Contorni</Tab>
+            <Tab>Bibite</Tab>
+          </SimpleGrid>
+        )}
       </TabList>
       <TabPanels>
         <TabPanel>
